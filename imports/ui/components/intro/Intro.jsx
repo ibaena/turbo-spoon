d360f861b3e21f4c4d549afdabea29b300b79e74
Ivan Baena <ivan.baena04@gmail.com>
2017-01-03 11:59:06 -0500

    sidelist added with transition

--- imports/ui/components/intro/Intro.jsx
+++ imports/ui/components/intro/Intro.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import SideList from '../sidelist/SideList.jsx';
+
 // Intro component
 export default class Intro extends Component {
   // Note: In ES6, constructor() === componentWillMount() in React ES5
@@ -9,9 +11,9 @@ export default class Intro extends Component {
   }
 
   componentDidMount(){
+    $('#intro-para').hide();
     let page = 1
     $(window).bind('mousewheel', function(e){
-
 
         if(e.originalEvent.wheelDelta /120 > 0) {
             $('.intro-container').show('slide',{direction:'up'},600);
@@ -25,11 +27,31 @@ export default class Intro extends Component {
             page = page +1;
         }
     });
+
+    setTimeout( () => {
+      $('#intro-para').animate({height:'toggle'},1100);
+    },400);
+
+
   }
   render() {
     return (
       <div className="intro-container">
+      <SideList />
         <div className="intro-overlay"></div>
+        <div className="container" id="intro-text">
+          <div className="col-sm-8 col-sm-offset-4" id="intro-para">
+            <div id="section-1">
+              <p className="intro-one-text">
+              <span className="highlight-text">I’M A WEB DEVELOPER </span>
+                from Pennsylvania living in Hoboken, New Jesery, where I work as a Front End Developer at Vision Creative Group. With web development, my strength lies in the Front End of website and app development.
+              </p>
+            </div>
+            <div id="section-2">
+              <p className="intro-two-text">If you want to know more about me, my work or if youʼre an Elder Scrolls Skyrim or want to talk Knicks basketball, feel free to contact me on ivan.baena04 [at] gmail.com.</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ imports/ui/components/sidelist/SideList.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// Intro component
+export default class SideList extends Component {
+  // Note: In ES6, constructor() === componentWillMount() in React ES5
+  constructor() {
+    super();
+
+  }
+
+  componentDidMount(){
+    $('.side-list-item').hide();
+    setTimeout( () => {
+      $('.side-list-item').animate({height:'toggle'},800);
+    },1200);
+  }
+  render() {
+    return (
+      <div className="sidelist-container">
+        <ul className="side-list">
+          <li className="side-list-item" id="side-1">Ivan Baena</li>
+          <li className="side-list-item" id="side-2">Front End Developer</li>
+          <li className="side-list-item" id="side-3">Pennsylvania &nbsp;-->&nbsp; New Jersey &nbsp;-->&nbsp; New York</li>
+        </ul>
+      </div>
+    );
+  }
+}
