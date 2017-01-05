30ca9c390a2d646e29f163eef4f1237bf7053b07
Ivan Baena <ivan.baena04@gmail.com>
2017-01-05 11:51:49 -0500

    working on outro container

--- imports/ui/components/outro/Contact.jsx
+++ imports/ui/components/outro/Contact.jsx
@@ -8,17 +8,36 @@ export default class Contact extends Component {
     super();
 
   }
+  getIcons() {
+    return [
+      { _id: 1, name:'Twitter', image:'/icons/twitter.png', src:'https://twitter.com/notthefakeib' },
+      { _id: 2, name:'Github', image:'/icons/git.png', src:'https://github.com/ibaena' },
+      { _id: 3, name:'Facebook', image:'/icons/fbook.png', src:'https://www.facebook.com/profile.php?id=9390215' },
+      { _id: 4, name:'Google+', image:'/icons/google.png', src:'https://www.linkedin.com/in/ivanbaena' },
+      { _id: 5, name:'Linkedin+', image:'/icons/linkedin.png', src:'https://www.linkedin.com/in/ivanbaena' },
 
+    ];
+  }
+  renderIcons() {
+    return this.getIcons().map((item) => (
+      <li className="contact-list-item" key={item._id}>
+        <a href={item.src} target="_blank">
+          <img src={item.image} className="contact-image grow" id={'contact-img-'+ item._id} />
+        </a>
+      </li>
+    ))
+  }
   componentDidMount(){
+
   }
   render() {
     return (
-      <div className="contact-container">
+      <div className="contact-container container">
+        <div className="col-md-12 icons-col">
           <ul className="contact-list">
-            <li className="contact-list-item">Social #1</li>
-            <li className="contact-list-item">Social #2</li>
-            <li className="contact-list-item">Social #3</li>
+            {this.renderIcons()}
           </ul>
+        </div>
       </div>
     );
   }
